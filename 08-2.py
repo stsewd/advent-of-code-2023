dfbe1fdb846a5cd2ed084609dd0045f3ede8cfe1
Santos Gallegos <stsewd@proton.me>
2023-12-08 14:40:07 -0500

    Solve day 8, part 1 and 2

---
+++ 08-2.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+import math
+from textwrap import dedent
+
+
+def solve(text: str) -> int:
+    lines = text.splitlines()
+    directions = lines[0]
+    nodes = {}
+    start_nodes = []
+    for line in lines[2:]:
+        node, _, neighbors = line.split(maxsplit=2)
+        neighbors = neighbors[1:-1].split(", ")
+        nodes[node] = neighbors
+        if node.endswith("A"):
+            start_nodes.append(node)
+
+    z_indexes = []
+    for start_node in start_nodes:
+        current_node = start_node
+        steps = 0
+        direction_index = 0
+        while not current_node.endswith("Z"):
+            direction = 0 if directions[direction_index] == "L" else 1
+            neighbors = nodes[current_node]
+            current_node = neighbors[direction]
+            direction_index = (direction_index + 1) % len(directions)
+            steps += 1
+        z_indexes.append(steps)
+
+    return math.lcm(*z_indexes)
+
+
+def main():
+    text = Path("08.txt").read_text()
+    print(solve(text))
+
+
+if __name__ == "__main__":
+    main()
+
+
+def test():
+    text = dedent(
+        """
+        LR
+
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)
+        """
+    ).strip()
+    assert solve(text) == 6
+
+    text = Path("08.txt").read_text()
+    assert solve(text) == 21165830176709
---
+++ 08.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+from textwrap import dedent
+
+
+def solve(text: str) -> int:
+    lines = text.splitlines()
+    directions = lines[0]
+    nodes = {}
+    for line in lines[2:]:
+        node, _, neighbors = line.split(maxsplit=2)
+        neighbors = neighbors[1:-1].split(", ")
+        nodes[node] = neighbors
+
+    current_node = "AAA"
+    direction_index = 0
+    total = 0
+    while current_node != "ZZZ":
+        neighbors = nodes[current_node]
+        direction = 0 if directions[direction_index] == "L" else 1
+        current_node = neighbors[direction]
+        direction_index = (direction_index + 1) % len(directions)
+        total += 1
+    return total
+
+
+def main():
+    text = Path("08.txt").read_text()
+    print(solve(text))
+
+
+if __name__ == "__main__":
+    main()
+
+
+def test():
+    text = dedent(
+        """
+        RL
+
+        AAA = (BBB, CCC)
+        BBB = (DDD, EEE)
+        CCC = (ZZZ, GGG)
+        DDD = (DDD, DDD)
+        EEE = (EEE, EEE)
+        GGG = (GGG, GGG)
+        ZZZ = (ZZZ, ZZZ)
+        """
+    ).strip()
+    assert solve(text) == 2
+
+    text = dedent(
+        """
+        LLR
+
+        AAA = (BBB, BBB)
+        BBB = (AAA, ZZZ)
+        ZZZ = (ZZZ, ZZZ)
+        """
+    ).strip()
+    assert solve(text) == 6
+
+    text = Path("08.txt").read_text()
+    assert solve(text) == 21409
